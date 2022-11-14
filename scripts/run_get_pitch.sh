1ec7419a5821a337c5022e45ecfef9bd2eb752a6
EliasEstevez <elias.ismael.estevez@estudiantat.upc.edu>
2022-11-14 15:46:33 +0100

    retoques en run_get_pitch.sh

--- scripts/run_get_pitch.sh
+++ scripts/run_get_pitch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-umaxnorm=${1:+-m $1}
+#umaxnorm=${1:+-m $1}
 # Put here the program (maybe with path)
-GETF0="get_pitch $umaxnorm" 
-
-
+GETF0="get_pitch --umaxnorm=0.40 --unorm=0.85 --umaxpot=5"   
+#$umaxnorm después de get_pitch 
 for fwav in pitch_db/train/*.wav; do
     ff0=${fwav/.wav/.f0}
     echo "$GETF0 $fwav $ff0 ----"
